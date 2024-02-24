0ff5c6d21a4807d1d85a5ade10ec5ff87ac0b219
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2024-02-24 16:05:00 +0330

    [FIX] segregation

--- src/Templates/ZalBase.php
+++ src/Templates/ZalBase.php
@@ -3,6 +3,7 @@
 namespace arashrasoulzadeh\Zal\Templates;
 
 use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
 
 define('GET', 'GET');
 define('POST', 'POST');
@@ -28,5 +29,47 @@ abstract class ZalBase
     public function method()
     {
         return $this->getRequest()->getMethod();
+    }
+
+    protected final function actionProxy($res)
+    {
+        /** @var Response $res */
+        $res->header('Access-Control-Max-Age', $this->cacheTimeout());
+        $res->headers->set('Cache-Control', 'max-age');
+        $res->header('X-Routed-By', 'Zal');
+        return $res;
+    }
+
+
+
+    public function cacheTimeout(): int
+    {
+        return 0;
+    }
+
+    public function cacheKey(): string
+    {
+        return $this::class;
+    }
+
+    public function param($key, $default = null)
+    {
+        if ($key == 'id') {
+            if (!is_null($this->id())) {
+                return $this->id();
+            }
+        }
+        return $this->getRequest()->$key ?? $default;
+    }
+
+    public function user()
+    {
+        $request = $this->getRequest();
+        $token = $request->bearerToken();
+        if ($token && $user = PersonalAccessToken::findToken($token)) {
+            return $user;
+        }
+
+        return null;
     }
 }
